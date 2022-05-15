6591221301630b25c1f3755e181604100f9d0a36
alexandervcc <alex.charco.25@gmail.com>
2022-05-15 16:30:44 -0500

    Pagination

--- src/main/java/acc/spring/jpa/jpapractice/model/Teacher.java
+++ src/main/java/acc/spring/jpa/jpapractice/model/Teacher.java
@@ -1,14 +1,10 @@
 package acc.spring.jpa.jpapractice.model;
 
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
--- src/main/java/acc/spring/jpa/jpapractice/repositories/ICourseRepository.java
+++ src/main/java/acc/spring/jpa/jpapractice/repositories/ICourseRepository.java
@@ -1,5 +1,7 @@
 package acc.spring.jpa.jpapractice.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import acc.spring.jpa.jpapractice.model.Course;
 @Repository
 public interface ICourseRepository extends JpaRepository<Course,Long> {
     
+    Page<Course> findByTitleContaining(String title, PageRequest pageRequest);
 }
